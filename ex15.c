0827ad5628e5b4985f4c590884020671077b053f
eugene3367 <dbwls2518@naver.com>
2022-10-13 02:43:46 +0900

    study

--- ex15.c
+++ ex15.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
-
-/* 두 개의 정수를 입력 받아 최대공약수(GCD)를 출력하는 프로그램을 작성하라
-while문 이용
-순서도를 반드시 만들 것.
-최대공약수란? 두 수 이상의 공통된 약수중에서 가장 큰 수
+/* 두개의 점수를 입력받아 최대 공약수(G,C,D)를 출력하는 프로그램을 작성하라
+(while문을 이용)
+순서도를 반드시 만들것.
+최대공약수란 두수 이상의 공통된 약수중에서 가장 큰수
 공통으로 두 수의 나머지가 0인(나누어 떨어지는) 수를 공약수로 하고,
-나누는 수로 증가시켜서 맨 나중까지 gcd에 남아 있는 수가 공약수 중에서
+나누는 수를 증가시켜서 맨 나중까지 gcd에 남아있는 수가 공약수 중에서
 가장 큰 값인 최대공약수가 된다.
-종료시점은 나누는 수가 두 수중에서 어느라도 더 크거나 같으면 종료
-
+종료시점은 나누는 수가 두 수중에서 어느 하나라도 더 크거나 같으면 종료
+24, 72 ---> 2, 3, 4, 6, 8...24
 */
 
 void main(){
-	
 	int a, b, divide=1, gcd=1;
-	
-	printf("number of first : ");	
+	printf("number of first : ");
 	scanf("%d", &a);
-	printf("number of second: ")
+	printf("number of second : ");
 	scanf("%d", &b);
-	
-	while(1){
-		if(a%divide==0 && b%divide==0){
-			gcd=divide;
-	
+	while(1){	//무한루프 1:참 0:거짓
+		if(a%divide==0 && b%divide==0)	gcd=divide;
+		if(divide>=a || divide>=b)	break;
 		divide++;
 	}
-	
+	printf("between %d and %d = GCD : %d\n", a, b, gcd);
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
